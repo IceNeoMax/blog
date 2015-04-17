632c681e99750d5cca70dfd7e5303a19dc6958ec
IceNeoMax <study4fun2011@gmail.com>
2015-04-17 12:16:25 +0700

    update package

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit915effc9efc5bd35da861f65401a38e1::getLoader();
+return ComposerAutoloaderInitdb707df062d60a271561c94bc825c7e9::getLoader();
